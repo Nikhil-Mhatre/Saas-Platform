5581d6a7c2561e4c266eadd6afb8d56dda5a3f41
Nikhil Mhatre <144405614+Nikhil-Mhatre@users.noreply.github.com>
2024-03-30 16:28:55 +0530

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+const page = () => (
+ <div className='h-auto sm:min-h-screen flex flex-col justify-center items-center'>
+  <h1
+   className='text-8xl bg-gradient-to-t from-blue-600
+   to-indigo-200 font-semibold text-transparent bg-clip-text'>
+   Hello World
+  </h1>
+  <p className='text-2xl mt-12 tracking-widest'>Welcome, Again</p>
+ </div>
+);
+
+export default page;
