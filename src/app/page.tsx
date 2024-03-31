90fbbfc3a5b8e0ff824c0133de00bab8c460b796
Nikhil Mhatre <nikhilmhatre703@gmail.com>
2024-03-31 12:05:09 +0530

    added Landing Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,132 @@
+import MaxWidthWrapper from '@/components/Max-width-wrapper';
+import { buttonVariants } from '@/components/ui/button';
+import { ArrowRight } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+
 const page = () => (
- <div className='h-auto sm:min-h-screen flex flex-col justify-center items-center'>
-  <h1
-   className='text-8xl bg-gradient-to-t from-blue-600
-   to-indigo-200 font-semibold text-transparent bg-clip-text'>
-   Hello World
-  </h1>
-  <p className='text-2xl mt-12 tracking-widest'>Welcome, Again</p>
- </div>
+  <>
+    <MaxWidthWrapper
+      className='mb-12 mt-28 sm:mt-40 flex flex-col items-center
+      justify-center text-center'>
+      <div
+        className='mx-auto mb-4 flex max-w-fit items-center justify-center
+        space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50'>
+        <p className='text-sm font-semibold text-gray-700'>
+          PDFMaestro is now public!
+        </p>
+      </div>
+      <h1
+        className='max-w-4xl text-5xl font-bold md:text-6xl
+        lg:text-7xl'>
+        Chat with your <span className='text-blue-600'>documents</span> in
+        seconds.
+      </h1>
+      <p className='mt-5 max-w-prose text-zinc-700 sm:text-lg'>
+        PDFMaestro allows you to have conversation with any PDF document. Simply
+        upload the PDF file and start asking questions.
+      </p>
+      <Link
+        className={buttonVariants({
+          size: 'lg',
+          className: 'mt-5',
+        })}
+        href={'/dashboard'}
+        target='_blank'>
+        Get Started <ArrowRight className='ml-2 h-5 w-5' />
+      </Link>
+    </MaxWidthWrapper>
+    {/* Value Proposition Section */}
+    <div>
+      {/* Dashboard Preview Image */}
+      <div>
+        <div className='mx-auto max-w-6xl px-6 lg:px-8'>
+          <div className='mt-16 flow-root sm:mt-24'>
+            <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
+              <Image
+                src='/dashboard-preview.jpg'
+                alt='product preview'
+                width={1364}
+                height={866}
+                quality={100}
+                className='rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* PDFMaestro Usage Title */}
+      <div className='mx-auto mb-12 mt-32 max-w-5xl sm:mt-56'>
+        <div className='mb-12 px-6 lg:px-8'>
+          <div className='mx-auto max-w-2xl sm:text-center'>
+            <h2 className='mt-2 font-bold text-4xl text-gray-900 sm:text-5xl'>
+              Start chatting in minutes
+            </h2>
+            <p className='mt-4 text-lg text-gray-600'>
+              Chatting to your PDF files has never been easier than with Quill.
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* steps of using PDFMaestro */}
+      <ol className='my-8 space-y-4 pt-8 md:px-12 md:flex md:space-x-12 md:space-y-0'>
+        <li className='md:flex-1'>
+          <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+            <span className='text-sm font-medium text-blue-600'>Step 1</span>
+            <span className='text-xl font-semibold'>
+              Sign up for an account
+            </span>
+            <span className='mt-2 text-zinc-700'>
+              Either starting out with a free plan or choose our{' '}
+              <Link
+                href='/pricing'
+                className='text-blue-700 underline underline-offset-2'>
+                pro plan
+              </Link>
+              .
+            </span>
+          </div>
+        </li>
+        <li className='md:flex-1'>
+          <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+            <span className='text-sm font-medium text-blue-600'>Step 2</span>
+            <span className='text-xl font-semibold'>Upload your PDF file</span>
+            <span className='mt-2 text-zinc-700'>
+              We&apos;ll process your file and make it ready for you to chat
+              with.
+            </span>
+          </div>
+        </li>
+        <li className='md:flex-1'>
+          <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+            <span className='text-sm font-medium text-blue-600'>Step 3</span>
+            <span className='text-xl font-semibold'>
+              Start asking questions
+            </span>
+            <span className='mt-2 text-zinc-700'>
+              It&apos;s that simple. Try out PDFMaestro today - it really takes
+              less than a minute.
+            </span>
+          </div>
+        </li>
+      </ol>
+      {/* Uploading Image */}
+      <div className='mx-auto max-w-6xl px-6 lg:px-8'>
+        <div className='mt-16 flow-root sm:mt-24'>
+          <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
+            <Image
+              src='/file-upload-preview.jpg'
+              alt='uploading preview'
+              width={1419}
+              height={732}
+              quality={100}
+              className='rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </>
 );
 
 export default page;
