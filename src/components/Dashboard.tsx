9c2b2e4b557e8245a42270da51b667da858656f2
Nikhil Mhatre <nikhilmhatre703@gmail.com>
2024-04-02 21:31:21 +0530

    added file Skeleton

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -4,7 +4,6 @@ import React, { useState } from 'react';
 import UploadButton from '@/components/UploadButton';
 import { trpc } from '@/lib/trpc/TRPC-Client';
 import { Ghost, Loader2, MessageSquare, Plus, Trash } from 'lucide-react';
-import Skeleton from 'react-loading-skeleton';
 import { Button } from './ui/button';
 import Link from 'next/link';
 import { format } from 'date-fns';
@@ -90,11 +89,12 @@ const Dashboard = () => {
             ))}
         </ul>
       ) : isLoading ? (
-        <Skeleton
-          height={100}
-          className='my-2'
-          count={3}
-        />
+        // <Skeleton
+        //   height={100}
+        //   className='my-2'
+        //   count={3}
+        // />
+        <FileSkeleton Num_of_Skeletons={9} />
       ) : (
         <div className='mt-16 flex flex-col items-center gap-2'>
           <Ghost className='h-8 w-8 text-zinc-800' />
@@ -103,6 +103,46 @@ const Dashboard = () => {
         </div>
       )}
     </main>
+  );
+};
+
+const FileSkeleton = ({ Num_of_Skeletons }: { Num_of_Skeletons: number }) => {
+  return (
+    <ul className='mt-8 grid grid-cols-1 gap-6 divide-y divide-zinc-200 md:grid-cols-2 lg:grid-cols-3'>
+      {Array.from({ length: Num_of_Skeletons }).map(() => (
+        <li className='col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow transition hover:shadow-lg'>
+          <div className='flex flex-col gap-2'>
+            <div className='pt-6 px-6 flex w-full items-center justify-between space-x-6'>
+              <div className='h-10 w-10 flex-shrink-0 rounded-full bg-gray-200' />
+              <div className='flex-1 truncate'>
+                <div className='flex items-center space-x-3'>
+                  <h3 className='truncate text-lg font-medium text-gray-200'>
+                    **************
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className='px-6 mt-4 grid grid-cols-3 place-items-center py-2 gap-6 text-xs text-zinc-500'>
+            <div className='flex items-center gap-2'>
+              {/* <Plus className='h-4 w-4' />
+                {format(new Date(file.createdAt), 'dd MMM yyyy')} */}
+              <div className='h-5 w-20 bg-gray-200' />
+            </div>
+
+            <div className='flex items-center gap-2'>
+              <div className='h-5 w-20 bg-gray-200' />
+            </div>
+
+            <Button
+              size='sm'
+              className='w-full bg-gray-200'
+              variant='secondary'></Button>
+          </div>
+        </li>
+      ))}
+    </ul>
   );
 };
 
